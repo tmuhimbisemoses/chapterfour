83b291ff08c023ae7794735aa6371057e83b8f96
tmuhimbisemoses <tumuhimbise.moses@gmail.com>
2016-09-20 20:52:16 +0300

    testing out if statements

---
+++ if.rb
@@ -0,0 +1,14 @@
+puts "Enter fare:"
+number=gets.to_i
+if (number == 1000)
+	puts "WEBALE KUSASULA!!"
+elsif (number > 1000)
+    balance = number - 1000
+    puts "WUNNO BALANCE WO" + balance.to_s
+else (number < 1000)
+    puts "YONGELAKO SSEBO"
+end
+
+
+
+	
